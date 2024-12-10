773f7fb18815dc4becf96ceced73ba7a305c1d20
boris <beskrovniibv@gmail.com>
2024-12-10 19:59:36 +0000

    Added solution for problem #2981

--- 2981/2981-1.py
+++ 2981/2981-1.py
@@ -2,14 +2,22 @@
 
 class Solution:
     def maximumLength(self, s: str) -> int:
-        pass
+        n = len(s) - 2
+        for l in range(n, 0, -1):
+            for i1 in range(len(s) - l - 1):
+                for i2 in range(i1 + 1, len(s)):
+                    for i3 in range(i2 + 1, len(s) - l + 1):
+                        if s[i1:i1 + l] == s[i2:i2 + l] == s[i3:i3 + l] and len(set(s[i1:i1 + l])) == 1:
+                            return l
+        return -1
 
 
 def main():
     examples = (
         ("aaaa", 2),
         ("abcdef", -1),
-        ("abcaba", 1)
+        ("abcaba", 1),
+        ("cccerrrecdcdccedecdcccddeeeddcdcddedccdceeedccecde", 2)
     )
     solution = Solution()
     for idx, example in enumerate(examples):
