3fa70e6b5b560dc8782b0e4607667e646eca331c
boris <beskrovniibv@gmail.com>
2024-12-12 10:49:25 +0000

    Added draft for problem #2558

--- 2558/2558-1.py
+++ 2558/2558-1.py
@@ -1,3 +1,15 @@
+#! /usr/bin/python python
+from typing import List
+
+
 class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
-        
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
