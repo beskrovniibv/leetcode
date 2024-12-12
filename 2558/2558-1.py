f825db2702498ef41cbb38a0aa70442747eecbbd
boris <beskrovniibv@gmail.com>
2024-12-12 11:02:01 +0000

    Added solution for problem #2558

--- 2558/2558-1.py
+++ 2558/2558-1.py
@@ -4,11 +4,34 @@ from typing import List
 
 class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
-        pass
+        result = sum(gifts)
+        gifts.sort()
+        for _ in range(k):
+            pile = gifts[-1]
+            leave = int(pile**0.5)
+            result -= pile - leave
+            gifts[-1] = leave
+            gifts.sort()
+        return result
 
 
 def main():
-    pass
+    examples = (
+        (
+            [25, 64, 9, 4, 100], 4, 29
+        ),
+        (
+            [1, 1, 1, 1], 4, 4
+        )
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        gifts, k, expected = example
+        got = solution.pickGifts(
+            gifts=gifts,
+            k=k
+        )
+        assert got == expected, f"Error in sample: {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
