cc2d3cd740cec3d456190b4046ed203325de4812
boris <beskrovniibv@gmail.com>
2024-12-13 08:56:03 +0000

    Added draft for problem #2593

--- 2593/2593-1.py
+++ 2593/2593-1.py
@@ -1,3 +1,16 @@
+#! /usr/bin/python python
+
+from typing import List
+
+
 class Solution:
     def findScore(self, nums: List[int]) -> int:
-        
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
