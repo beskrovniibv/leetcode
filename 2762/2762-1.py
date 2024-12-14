71a4a1df999b1fc9bf19dba00b4991359f686414
boris <beskrovniibv@gmail.com>
2024-12-14 07:28:30 +0000

    Improve draft for problem #2762

--- 2762/2762-1.py
+++ 2762/2762-1.py
@@ -9,7 +9,21 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            [5, 4, 2, 4], 8
+        ),
+        (
+            [1, 2, 3], 6
+        )
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        nums, expected = example
+        got = solution.continuousSubarrays(
+            nums=nums
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
