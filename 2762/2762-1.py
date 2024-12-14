12bef0d781cd4b211ebb1f2ac539fede415bd805
boris <beskrovniibv@gmail.com>
2024-12-14 07:16:11 +0000

    Added draft for problem #2762

--- 2762/2762-1.py
+++ 2762/2762-1.py
@@ -1,3 +1,16 @@
+#! /usr/bin/python python
+
+from typing import List
+
+
 class Solution:
     def continuousSubarrays(self, nums: List[int]) -> int:
-        
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
