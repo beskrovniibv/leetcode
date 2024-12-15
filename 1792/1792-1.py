3a9a5e790d5f5ca171682e7608c329524cc0fa14
boris <beskrovniibv@gmail.com>
2024-12-15 14:21:56 +0000

    Add draft for problem #1792

--- 1792/1792-1.py
+++ 1792/1792-1.py
@@ -9,7 +9,22 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            [[1, 2], [3, 5], [2, 2]], 2, 0.78333
+        ),
+        (
+            [[2, 4], [3, 9], [4, 5], [2, 10]], 4, 0.53485
+        )
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        classes, extraStudents, expected = example
+        got = solution.maxAverageRatio(
+            classes=classes,
+            extraStudents=extraStudents
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
