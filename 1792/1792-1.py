d282cb3daabb6b0701297f600fb6a8082ff9a132
boris <beskrovniibv@gmail.com>
2024-12-15 14:15:06 +0000

    Add draft for problem #1792

---
+++ 1792/1792-1.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python python
+
+from typing import List
+
+
+class Solution:
+    def maxAverageRatio(self, classes: List[List[int]], extraStudents: int) -> float:
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
