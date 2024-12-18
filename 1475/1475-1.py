ccc79ec0de1ac8d9096e3471f9e53108764ff4be
boris <beskrovniibv@gmail.com>
2024-12-18 07:36:00 +0000

    Improve draft for problem #1475

--- 1475/1475-1.py
+++ 1475/1475-1.py
@@ -9,7 +9,24 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            [8, 4, 6, 2, 3],  [4, 2, 4, 2, 3]
+        ),
+        (
+            [1, 2, 3, 4, 5],  [1, 2, 3, 4, 5]
+        ),
+        (
+            [10, 1, 1, 6],  [9, 0, 1, 6]
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        prices, expected = example
+        got = solution.finalPrices(
+            prices=prices
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
