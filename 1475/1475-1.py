1b055f77f8fbd8ec544f710d72851b0bd5a6dfac
boris <beskrovniibv@gmail.com>
2024-12-18 07:28:40 +0000

    Add draft for problem #1475

--- 1475/1475-1.py
+++ 1475/1475-1.py
@@ -1,3 +1,16 @@
+#! /usr/bin/python python
+
+from typing import List
+
+
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
-        
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
