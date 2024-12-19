223983399bcb02353fd83228d3df9d20cb3feb8d
boris <beskrovniibv@gmail.com>
2024-12-19 07:39:45 +0000

    Improve draft for problem #769

--- 769/769-1.py
+++ 769/769-1.py
@@ -9,7 +9,21 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            [4, 3, 2, 1, 0], 1
+        ),
+        (
+            [1, 0, 2, 3, 4], 4
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        arr, expected = example
+        got = solution.maxChunksToSorted(
+            arr=arr
+        )
+        assert got == expected, f"Error in samples {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
