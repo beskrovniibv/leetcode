b14dc4d0789629f0e35126ed9a34926c80666153
boris <beskrovniibv@gmail.com>
2024-12-19 07:28:02 +0000

    Add draft for problem #769

--- 769/769-1.py
+++ 769/769-1.py
@@ -1,3 +1,16 @@
+#! /usr/bin/python pyton
+
+from typing import List
+
+
 class Solution:
     def maxChunksToSorted(self, arr: List[int]) -> int:
-        
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
