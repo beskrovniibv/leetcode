fb3d93a855899c6f9afa29ff8b7707b05596b0f5
boris <beskrovniibv@gmail.com>
2024-12-20 05:43:29 +0000

    Add draft for problem #2415

---
+++ 2415/2415-1.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
