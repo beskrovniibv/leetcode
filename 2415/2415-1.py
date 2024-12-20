64004a88b7869ae0fdc24f89a01a1b7085c37469
boris <beskrovniibv@gmail.com>
2024-12-20 20:18:30 +0000

    Add solution for problem #2415

--- 2415/2415-1.py
+++ 2415/2415-1.py
@@ -1,5 +1,5 @@
 # Definition for a binary tree node.
-from typing import Optional
+from typing import List, Optional
 
 
 class TreeNode:
@@ -11,12 +11,47 @@ class TreeNode:
 
 class Solution:
     def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        pass
+        def dfs(left: Optional[TreeNode], right: Optional[TreeNode], isOddLevel: bool) -> None:
+            if not left:
+                return
+            if isOddLevel:
+                left.val, right.val = right.val, left.val
+            dfs(left.left, right.right, not isOddLevel)
+            dfs(left.right, right.left, not isOddLevel)
+
+        dfs(root.left, root.right, True)
+        return root
+
+
+def getTreeFromList(treeList: List[int]) -> TreeNode:
+    pass
+
+
+def getListFromTree(root: TreeNode) -> List[int]:
+    pass
 
 
 def main():
-    pass
+    examples = (
+        (
+            [2, 3, 5, 8, 13, 21, 34], [2, 5, 3, 8, 13, 21, 34]
+        ),
+        (
+            [7, 13, 11], [7, 11, 13]
+        ),
+        (
+            [0, 1, 2, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2], [0, 2, 1, 0, 0, 0, 0, 2, 2, 2, 2, 1, 1, 1, 1]
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        treeList, expected = example
+        root = getTreeFromList(treeList)
+        got = solution.reverseOddLevels(root)
+        answer = getListFromTree(got)
+        assert answer == expected, f"Error in sample {idx + 1}, expected {expected}, got {answer}"
 
 
 if __name__ == "__main__":
     main()
+    
