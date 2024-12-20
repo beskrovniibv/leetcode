5c821923902a1321bfa0a381fbc3f2dd9f539d40
boris <beskrovniibv@gmail.com>
2024-12-20 22:08:06 +0000

    Add draft for problem #4

--- 4/4-1.py
+++ 4/4-1.py
@@ -1,3 +1,13 @@
+from typing import List
+
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
