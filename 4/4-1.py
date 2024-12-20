d1d5caa674241f21fedb4e6bcf14c2698507be29
boris <beskrovniibv@gmail.com>
2024-12-20 22:14:17 +0000

    Add solution for problem #4

--- 4/4-1.py
+++ 4/4-1.py
@@ -3,7 +3,38 @@ from typing import List
 
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        pass
+        s = 0
+        n1 = len(nums1)
+        n2 = len(nums2)
+        m = ((n1 + n2) // 2,) if (n1 + n2) & 1 == 1 else ((n1 + n2) // 2 - 1, (n1 + n2) // 2)
+        n = len(m)
+        i, j = 0, 0
+        i1 = 0
+        i2 = 0
+        while j < n:
+            if i1 < len(nums1) and i2 < len(nums2):
+                if nums1[i1] < nums2[i2]:
+                    if i == m[j]:
+                        s += nums1[i1]
+                        j += 1
+                    i1 += 1
+                else:
+                    if i == m[j]:
+                        s += nums2[i2]
+                        j += 1
+                    i2 += 1
+            elif i1 < len(nums1):
+                if i == m[j]:
+                    s += nums1[i1]
+                    j += 1
+                i1 += 1
+            elif i2 < len(nums2):
+                if i == m[j]:
+                    s += nums2[i2]
+                    j += 1
+                i2 += 1
+            i += 1
+        return s/n
 
 
 def main():
