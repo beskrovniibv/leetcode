102e80f39f58aa26bd5a6e7a4133078c2e351fc1
boris <beskrovniibv@gmail.com>
2024-12-20 22:13:47 +0000

    Improve draft for problem #4

--- 4/4-1.py
+++ 4/4-1.py
@@ -7,8 +7,28 @@ class Solution:
 
 
 def main():
-    pass
-
+    examples = (
+        (
+            [1, 3], [2], 2.00000
+        ),
+        (
+            [1, 2], [3, 4], 2.50000
+        ),
+        (
+            [0, 0], [0, 0], 0.0
+        ),
+        (
+            [2, 2, 4, 4], [2, 2, 2, 4, 4], 2.0
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        nums1, nums2, expected = example
+        got = solution.findMedianSortedArrays(
+            nums1=nums1,
+            nums2=nums2
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}"
 
 if __name__ == "__main__":
     main()
