3b9ca3e70efac3fe9040324e525e40890571c01f
boris <beskrovniibv@gmail.com>
2024-12-21 15:44:00 +0000

    Add draft for problem #2872

--- 2872/2872-1.py
+++ 2872/2872-1.py
@@ -1,3 +1,14 @@
+from typing import List
+
+
 class Solution:
     def maxKDivisibleComponents(self, n: int, edges: List[List[int]], values: List[int], k: int) -> int:
-        
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
