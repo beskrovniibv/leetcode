6626b777ce1ec4a00cde1ce6bff4824111886003
boris <beskrovniibv@gmail.com>
2024-12-22 12:49:23 +0000

    Add draft for problem #2940

--- 2940/2940-1.py
+++ 2940/2940-1.py
@@ -1,3 +1,16 @@
+#! /usr/bin/python python
+
+from typing import List
+
+
 class Solution:
     def leftmostBuildingQueries(self, heights: List[int], queries: List[List[int]]) -> List[int]:
-        
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
