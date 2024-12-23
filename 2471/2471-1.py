ce19fae1b6713f6ae27dc305928031919c493491
boris <beskrovniibv@gmail.com>
2024-12-23 17:39:20 +0000

    Add draft for problem #2471

---
+++ 2471/2471-1.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python python
+
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def minimumOperations(self, root: Optional[TreeNode]) -> int:
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
