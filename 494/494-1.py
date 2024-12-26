08052a9050bf1d1d5d2f57b558b2dccd0c9013d5
boris <beskrovniibv@gmail.com>
2024-12-26 20:32:10 +0000

    Add draft for problem #494

--- 494/494-1.py
+++ 494/494-1.py
@@ -1,3 +1,14 @@
+#! /usr/bin/python python
+
+
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
