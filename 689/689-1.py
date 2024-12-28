1a5186fe106f01ec6d8db4fe3e41cdda6c61caef
boris <beskrovniibv@gmail.com>
2024-12-28 16:07:50 +0000

    Add draft for problem #689

---
+++ 689/689-1.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python python python3
+
+from typing import List
+
+
+class Solution:
+    def maxSumOfThreeSubarrays(self, nums: List[int], k: int) -> List[int]:
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
