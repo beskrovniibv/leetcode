8b8ccdc8295e417338883ada1295f93d72c5c3a1
boris <beskrovniibv@gmail.com>
2024-12-30 19:10:33 +0000

    Improve draft for problem #2466

--- 2466/2466-1.py
+++ 2466/2466-1.py
@@ -6,7 +6,24 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            3, 3, 1, 1, 8
+        ),
+        (
+            2, 3, 1, 2, 5
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        low, high, zero, one, expected = example
+        got = solution.countGoodStrings(
+            low=low,
+            high=high,
+            zero=zero,
+            one=one
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
