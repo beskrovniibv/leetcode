3cc3b263a6b82e94f13fd576512820a3f1164014
boris <beskrovniibv@gmail.com>
2024-12-30 19:07:06 +0000

    Add draft for problem #2466

--- 2466/2466-1.py
+++ 2466/2466-1.py
@@ -1,3 +1,13 @@
+#! /usr/bin/python python pyton3
+
 class Solution:
     def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
-        
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
