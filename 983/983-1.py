fdd69407c9c11b4146b60789a764c706390f7b32
boris <beskrovniibv@gmail.com>
2024-12-31 03:36:20 +0000

    Improve solution for #983

--- 983/983-1.py
+++ 983/983-1.py
@@ -9,7 +9,22 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            [1, 4, 6, 7, 8, 20], [2, 7, 15], 11
+        ),
+        (
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 30, 31], [2, 7, 15], 17
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        days, costs, expected = example
+        got = solution.mincostTickets(
+            days=days,
+            costs=costs
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
