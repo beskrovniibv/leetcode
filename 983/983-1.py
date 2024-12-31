dada1250633ddc04d58943d997894976304832b2
boris <beskrovniibv@gmail.com>
2024-12-31 03:26:01 +0000

    Add draft for problem #983

---
+++ 983/983-1.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python python python3
+
+from typing import List
+
+
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
