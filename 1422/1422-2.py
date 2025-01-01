489469b1595d4bf7c06dc7de4d87554d3ff3ebba
boris <beskrovniibv@gmail.com>
2025-01-01 21:07:06 +0000

    Add solution for problem #1422

--- 1422/1422-2.py
+++ 1422/1422-2.py
@@ -2,11 +2,39 @@
 
 class Solution:
     def maxScore(self, s: str) -> int:
-        pass
+        n = len(s)
+        psumz = [0] + [0]*n
+        psumo = [0] + [0]*n
+        for i in range(1, n + 1):
+            psumz[i] = psumz[i - 1] + (1 if s[i - 1] == "0" else 0)
+            psumo[i] = psumo[i - 1] + (1 if s[i - 1] == "1" else 0)
+        result = 0
+        for i in range(n - 1):
+            ans0 = psumz[i + 1] - psumz[0]
+            ans1 = psumo[n] - psumo[i + 1]
+            result = max(result, ans0 + ans1)
+        return result
 
 
 def main():
-    pass
+    examples = (
+        (
+            "011101", 5
+        ),
+        (
+            "00111", 5
+        ),
+        (
+            "1111", 3
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        s, expected = example
+        got = solution.maxScore(
+            s=s
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
