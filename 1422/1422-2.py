d23e7f5768fdc302a4491d3a3dcdf0407336d91f
boris <beskrovniibv@gmail.com>
2025-01-01 21:11:05 +0000

    Improve solution for problem #1422

--- 1422/1422-2.py
+++ 1422/1422-2.py
@@ -3,14 +3,13 @@
 class Solution:
     def maxScore(self, s: str) -> int:
         n = len(s)
-        psumz = [0] + [0]*n
         psumo = [0] + [0]*n
         for i in range(1, n + 1):
-            psumz[i] = psumz[i - 1] + (1 if s[i - 1] == "0" else 0)
             psumo[i] = psumo[i - 1] + (1 if s[i - 1] == "1" else 0)
         result = 0
+        ans0 = 0
         for i in range(n - 1):
-            ans0 = psumz[i + 1] - psumz[0]
+            ans0 += (1 if s[i] == "0" else 0)
             ans1 = psumo[n] - psumo[i + 1]
             result = max(result, ans0 + ans1)
         return result
