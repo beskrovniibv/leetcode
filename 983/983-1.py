d34919d5e3cec2b0426ff20d6bae5e517c05c198
boris <beskrovniibv@gmail.com>
2025-01-01 20:37:13 +0000

    Add solution for problem #983

--- 983/983-1.py
+++ 983/983-1.py
@@ -5,7 +5,20 @@ from typing import List
 
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
-        pass
+        n = days[-1]
+        dp = [0] * (n + 1)
+        i = 0
+        for day in range(1, n + 1):
+            if day < days[i]:
+                dp[day] = dp[day - 1]
+            else:
+                i += 1
+                dp[day] = min(
+                    dp[day - 1] + costs[0],
+                    dp[max(0, day - 7)] + costs[1],
+                    dp[max(0, day - 30)] + costs[2],
+                )
+        return dp[n]
 
 
 def main():
