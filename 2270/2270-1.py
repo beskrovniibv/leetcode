0a931bee7012ba1550c1771b5f932c3c82f67672
boris <beskrovniibv@gmail.com>
2025-01-03 07:13:43 +0000

    Add draft for problem #2270

---
+++ 2270/2270-1.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python python python3
+
+from typing import List
+
+
+class Solution:
+    def waysToSplitArray(self, nums: List[int]) -> int:
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
