cb84a7c01e955b5ddc5445266fc6e80a707971cb
boris <beskrovniibv@gmail.com>
2025-01-04 02:47:09 +0000

    Add draft for problem #1930

--- 1930/1930-1.py
+++ 1930/1930-1.py
@@ -1,3 +1,13 @@
+#! /usr/bin/python python python3
+
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-        
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
