6b2f42c63355352ec48c2cb82c8dbaa6af21a178
boris <beskrovniibv@gmail.com>
2025-01-05 15:46:13 +0000

    Add solution for problem #2381 (TL)

--- 2381/2381-1.py
+++ 2381/2381-1.py
@@ -1,11 +1,25 @@
 #! /usr/bin/python python python3
 
+from string import ascii_lowercase
 from typing import List
 
 
 class Solution:
+    def shift(self, s: str, shifts: List[int]) -> str:
+        abc = list(ascii_lowercase)
+        result = list(s)
+        for i, sh in enumerate(shifts):
+            result[i] = abc[(ord(result[i]) + sh - ord("a")) % len(abc)]
+        return ''.join(result)
+
     def shiftingLetters(self,  s: str,  shifts: List[List[int]]) -> str:
-        pass
+        n = len(s)
+        total = [0]*n
+        for shift in shifts:
+            l, r, d = shift
+            for i in range(l, r + 1):
+                total[i] = total[i] + (1 if d == 1 else -1)
+        return self.shift(s, total)
 
 
 def main():
