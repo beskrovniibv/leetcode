7bb6a9a92bcaaaaee14cc18e42ac7599c63f2323
boris <beskrovniibv@gmail.com>
2025-01-05 14:01:24 +0000

    Add draft for problem #2381

--- 2381/2381-1.py
+++ 2381/2381-1.py
@@ -1,3 +1,31 @@
+#! /usr/bin/python python python3
+
+from typing import List
+
+
 class Solution:
-    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
-        
+    def shiftingLetters(self,  s: str,  shifts: List[List[int]]) -> str:
+        pass
+
+
+def main():
+    examples = (
+        (
+            "abc", [[0, 1, 0], [1, 2, 1], [0, 2, 1]], "ace"
+        ),
+        (
+            "dztz",  [[0,  0,  0],  [1,  1,  1]],  "catz"
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        s, shifts, expected = example
+        got = solution.shiftingLetters(
+            s=s,
+            shifts=shifts
+        )
+        assert got == expected, f"Error in {idx + 1} sample: expected {expected}, got {got}."
+
+
+if __name__ == "__main__":
+    main()
