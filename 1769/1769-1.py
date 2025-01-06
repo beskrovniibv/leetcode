fa9847f3febc2659f0c0b06ff2bf3a2c8a60c795
boris <beskrovniibv@gmail.com>
2025-01-06 18:14:10 +0000

    Add draft for problem #1769

--- 1769/1769-1.py
+++ 1769/1769-1.py
@@ -9,7 +9,21 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            "110", [1, 1, 3]
+        ),
+        (
+            "001011", [11, 8, 5, 4, 3, 4]
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        boxes, expected = example
+        got = solution.minOperations(
+            boxes=boxes
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
