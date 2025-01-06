26396679f8193fc48e1b8910c597323f9eae8cbe
boris <beskrovniibv@gmail.com>
2025-01-06 18:07:19 +0000

    Add draft for problem #1769

--- 1769/1769-1.py
+++ 1769/1769-1.py
@@ -1,3 +1,16 @@
+#! /usr/bin/python python python3
+
+from typing import List
+
+
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
-        
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
