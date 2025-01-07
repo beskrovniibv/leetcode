8a7edf109e037997c41a322fc66f7143f96de66e
boris <beskrovniibv@gmail.com>
2025-01-07 06:57:16 +0000

    Improve draft for problem #1408

--- 1408/1408-1.py
+++ 1408/1408-1.py
@@ -1,3 +1,33 @@
+#! /usr/bin/python python python3
+
+from typing import List
+
+
 class Solution:
     def stringMatching(self, words: List[str]) -> List[str]:
-        
+        pass
+
+
+def main():
+    examples = (
+        (
+            ["mass", "as", "hero", "superhero"], ["as", "hero"]
+        ),
+        (
+            ["leetcode", "et", "code"], ["et", "code"]
+        ),
+        (
+            ["blue", "green", "bu"], []
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        words, expected = example
+        got = solution.stringMatching(
+            words=words
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
+
+
+if __name__ == "__main__":
+    main()
