72453c71a09d25fc90f86c82970ba527dd8f0766
boris <beskrovniibv@gmail.com>
2025-01-08 00:11:45 +0000

    Add draft for problem #3042

--- 3042/3042-1.py
+++ 3042/3042-1.py
@@ -1,3 +1,16 @@
+#! /usr/bin/python python python3
+
+from typing import List
+
+
 class Solution:
     def countPrefixSuffixPairs(self, words: List[str]) -> int:
-        
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
