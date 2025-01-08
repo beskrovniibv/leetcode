198c68b97f18decee70f48d19b0a6818ef428705
boris <beskrovniibv@gmail.com>
2025-01-08 00:19:57 +0000

    Improve draft for problem #3042

--- 3042/3042-1.py
+++ 3042/3042-1.py
@@ -9,7 +9,24 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            ["a", "aba", "ababa", "aa"], 4
+        ),
+        (
+            ["pa", "papa", "ma", "mama"], 2
+        ),
+        (
+            ["abab", "ab"], 0
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        words, expected = example
+        got = solution.countPrefixSuffixPairs(
+            words=words
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
