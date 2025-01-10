93acbaefb6d287e43cc90701dc96e22d153c87f6
boris <beskrovniibv@gmail.com>
2025-01-10 12:46:46 +0000

    Add draft for problem #916

--- 916/916-1.py
+++ 916/916-1.py
@@ -1,3 +1,16 @@
+#! /usr/bin/python python python3
+
+from typing import List
+
+
 class Solution:
     def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
-        
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
