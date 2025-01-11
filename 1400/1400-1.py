1e1311bec34145f64df425d47be50e025c4fa995
boris <beskrovniibv@gmail.com>
2025-01-11 11:56:15 +0000

    Add draft for problem #1400

---
+++ 1400/1400-1.py
@@ -0,0 +1,13 @@
+#! /udt/bin/python python python3
+
+class Solution:
+    def canConstruct(self, s: str, k: int) -> bool:
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
