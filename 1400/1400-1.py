588216b9d943e264916797f2a61c0aaf431f0f18
boris <beskrovniibv@gmail.com>
2025-01-11 12:04:14 +0000

    Improve draft for problem #1400

--- 1400/1400-1.py
+++ 1400/1400-1.py
@@ -1,4 +1,4 @@
-#! /udt/bin/python python python3
+#! /usr/bin/python python python3
 
 class Solution:
     def canConstruct(self, s: str, k: int) -> bool:
@@ -6,7 +6,25 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            "annabelle", 2, True
+        ),
+        (
+            "leetcode", 3, False
+        ),
+        (
+            "true", 4, True
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        s, k, expected = example
+        got = solution.canConstruct(
+            s=s,
+            k=k
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
