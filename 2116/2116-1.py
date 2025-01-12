db52ae2bf25f39dfabe8e1814f7402129347bb22
boris <beskrovniibv@gmail.com>
2025-01-12 01:22:33 +0000

    Add solution for problem #2116

--- 2116/2116-1.py
+++ 2116/2116-1.py
@@ -2,16 +2,49 @@
 
 class Solution:
     def canBeValid(self, s: str, locked: str) -> bool:
-        pass
+        opened = []
+        unlocked = []
+
+        for i, (ch, state) in enumerate(zip(s, locked)):
+            if state == "0":
+                unlocked.append(i)
+            elif ch == "(":
+                opened.append(i)
+            else:
+                if opened:
+                    opened.pop()
+                elif unlocked:
+                    unlocked.pop()
+                else:
+                    return False
+        while opened and unlocked and opened[-1] < unlocked[-1]:
+            opened.pop()
+            unlocked.pop()
+        return not opened and len(unlocked) & 1 == 0
 
 
 def main():
     examples = (
         (
+            "(())", "0101", True
+        ),
+        (
+            "((()(()()))()((()()))))()((()(()", "10111100100101001110100010001001", True
+        ),
+        (
+            ")(", "00", True
+        ),
+        (
             "))()))", "010100", True
         ),
         (
             "()()", "0000", True
+        ),
+        (
+            ")", "0", False
+        ),
+        (
+            "()))", "0010", True
         ),
         (
             ")", "0", False
