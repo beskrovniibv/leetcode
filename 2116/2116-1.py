cd389bce6c3e686308f3da771fbee84a596febd2
boris <beskrovniibv@gmail.com>
2025-01-12 00:14:53 +0000

    Add draft for problem #2116

--- 2116/2116-1.py
+++ 2116/2116-1.py
@@ -1,3 +1,13 @@
+#! /usr/bin/python python python3
+
 class Solution:
     def canBeValid(self, s: str, locked: str) -> bool:
-        
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
