728c64437a51802a2cb973a554e20c94300a1355
boris <beskrovniibv@gmail.com>
2025-01-14 12:26:35 +0000

    Improve draft for problem #2657

--- 2657/2657-1.py
+++ 2657/2657-1.py
@@ -9,7 +9,22 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            [1, 3, 2, 4], [3, 1, 2, 4], [0, 2, 3, 4]
+        ),
+        (
+            [2, 3, 1], [3, 1, 2], [0, 1, 3]
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        A, B, expected = example
+        got = solution.findThePrefixCommonArray(
+            A=A,
+            B=B
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
