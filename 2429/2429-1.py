a6d5bf8e26f896e66ba46da87721d3192699cf09
boris <beskrovniibv@gmail.com>
2025-01-15 20:53:45 +0000

    Add draft for problem #2429

--- 2429/2429-1.py
+++ 2429/2429-1.py
@@ -1,3 +1,13 @@
+#! /usr/bin/python python python3
+
 class Solution:
     def minimizeXor(self, num1: int, num2: int) -> int:
-        
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
