4c2aea6f04cb46ed22a64df828068267c73325fa
boris <beskrovniibv@gmail.com>
2025-01-16 00:24:01 +0000

    Add solution for problem #2429

--- 2429/2429-1.py
+++ 2429/2429-1.py
@@ -2,11 +2,48 @@
 
 class Solution:
     def minimizeXor(self, num1: int, num2: int) -> int:
-        pass
+        n = bin(num2).count('1')
+        num1_b = bin(num1)
+        result_b = ["0"]*len(num1_b[2:])
+        for i, c in enumerate(num1_b[2:]):
+            if c == "1":
+                result_b[i] = "1"
+                n -= 1
+            else:
+                result_b[i] = "0"
+            if n == 0:
+                break
+        for i in range(len(result_b) - 1, -1, -1):
+            if n == 0:
+                break
+            if result_b[i] == "0":
+                result_b[i] = "1"
+                n -= 1
+        result_b = ["1"]*n + result_b
+        result = int("".join(result_b), 2)
+        return result
 
 
 def main():
-    pass
+    examples = (
+        (
+            25, 72, 24
+        ),
+        (
+            3, 5, 3
+        ),
+        (
+            1, 12, 3
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        num1, num2, expected = example
+        got = solution.minimizeXor(
+            num1=num1,
+            num2=num2
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
