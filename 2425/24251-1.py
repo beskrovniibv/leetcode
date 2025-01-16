6bd9eb58ccf7d309bd6b8094032cd8555a6633d7
boris <beskrovniibv@gmail.com>
2025-01-16 00:36:01 +0000

    Improve draft for problem #2425

--- 2425/24251-1.py
+++ 2425/24251-1.py
@@ -9,7 +9,22 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            [2, 1, 3], [10, 2, 5, 0], 13
+        ),
+        (
+            [1, 2], [3, 4], 0
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        nums1, nums2, expected = example
+        got = solution.xorAllNums(
+            nums1=nums1,
+            nums2=nums2
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
