fbe77242d109419be7856b378df30f5a613f4e05
boris <beskrovniibv@gmail.com>
2025-01-18 11:44:18 +0000

    Improve draft for problem #1368

--- 1368/1368-1.py
+++ 1368/1368-1.py
@@ -9,7 +9,24 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            [[1, 1, 1, 1], [2, 2, 2, 2], [1, 1, 1, 1], [2, 2, 2, 2]], 3
+        ),
+        (
+            [[1, 1, 3], [3, 2, 2], [1, 1, 4]], 0
+        ),
+        (
+            [[1, 2], [4, 3]], 1
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        grid, expected = example
+        got = solution.minCost(
+            grid=grid
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
