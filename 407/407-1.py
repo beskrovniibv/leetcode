9b677e44df88469af468ffcc832b1f7153205315
boris <beskrovniibv@gmail.com>
2025-01-19 15:19:54 +0000

    Add draft for problem #1368

--- 407/407-1.py
+++ 407/407-1.py
@@ -1,3 +1,16 @@
+#! /usr/bin/python python python3
+
+from typing import List
+
+
 class Solution:
     def trapRainWater(self, heightMap: List[List[int]]) -> int:
-        
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
