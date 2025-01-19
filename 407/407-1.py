233772fffd3a40a31af6470bfdb56dd923bebd33
boris <beskrovniibv@gmail.com>
2025-01-19 15:22:59 +0000

    Improve draft for problem #1368

--- 407/407-1.py
+++ 407/407-1.py
@@ -9,7 +9,21 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            [[1, 4, 3, 1, 3, 2], [3, 2, 1, 3, 2, 4], [2, 3, 3, 2, 3, 1]], 4
+        ),
+        (
+            [[3, 3, 3, 3, 3], [3, 2, 2, 2, 3], [3, 2, 1, 2, 3], [3, 2, 2, 2, 3], [3, 3, 3, 3, 3]], 10
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        heightMap, expected = example
+        got = solution.trapRainWater(
+            heightMap=heightMap
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
