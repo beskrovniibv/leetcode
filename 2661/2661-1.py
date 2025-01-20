33b091b3b3ab6330b711d997b99e4174c74cc9c6
boris <beskrovniibv@gmail.com>
2025-01-20 00:22:12 +0000

    Improve draft for problem #2661

--- 2661/2661-1.py
+++ 2661/2661-1.py
@@ -9,7 +9,22 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            [1, 3, 4, 2], [[1, 4], [2, 3]], 2
+        ),
+        (
+            [2, 8, 7, 4, 1, 3, 5, 6, 9], [[3, 2, 5], [1, 4, 6], [8, 7, 9]], 3
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        arr, mat, expected = example
+        got = solution.firstCompleteIndex(
+            arr=arr,
+            mat=mat,
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 
