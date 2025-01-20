e87630e5a135ce241350967abfd51890bdbbb139
boris <beskrovniibv@gmail.com>
2025-01-20 00:18:08 +0000

    Add draft for problem #2661

---
+++ 2661/2661-1.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python python python3
+
+from typing import List
+
+
+class Solution:
+    def firstCompleteIndex(self, arr: List[int], mat: List[List[int]]) -> int:
+        pass
+
+
+def main():
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
