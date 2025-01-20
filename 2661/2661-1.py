9e0c312a2f27ec9df4aa0d72f67b18fa42f336d2
boris <beskrovniibv@gmail.com>
2025-01-20 00:34:43 +0000

    Add solution for problem #2661

--- 2661/2661-1.py
+++ 2661/2661-1.py
@@ -5,11 +5,25 @@ from typing import List
 
 class Solution:
     def firstCompleteIndex(self, arr: List[int], mat: List[List[int]]) -> int:
-        pass
+        num, row_num, col_num = len(arr), len(mat), len(mat[0])
+        rows, cols = [0]*row_num, [0]*col_num
+        cells = {}
+        for row in range(row_num):
+            for col in range(col_num):
+                cells[mat[row][col]] = (row, col)
+        for i in range(num):
+            r, c = cells[arr[i]]
+            rows[r] += 1
+            cols[c] += 1
+            if rows[r] == col_num or cols[c] == row_num:
+                return i
 
 
 def main():
     examples = (
+        (
+            [1, 4, 5, 2, 6, 3], [[4, 3, 5], [1, 2, 6]], 1
+        ),
         (
             [1, 3, 4, 2], [[1, 4], [2, 3]], 2
         ),
@@ -25,7 +39,6 @@ def main():
             mat=mat,
         )
         assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
-
 
 
 if __name__ == "__main__":
