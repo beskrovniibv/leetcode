79e794414226c96a5acd00ad3858cb3cc429aa93
boris <beskrovniibv@gmail.com>
2025-01-21 17:49:39 +0000

    Improve draft for problem #2017

--- 2017/2017-1.py
+++ 2017/2017-1.py
@@ -9,7 +9,24 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            [[2, 5, 4], [1, 5, 1]], 4
+        ),
+        (
+            [[3, 3, 1], [8, 5, 2]], 4
+        ),
+        (
+            [[1, 3, 1, 15], [1, 3, 3, 1]], 7
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        grid, expected = example
+        got = solution.gridGame(
+            grid=grid
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
