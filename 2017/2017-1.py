b7042a08261d33920ebc8861df9a357bc6c59592
boris <beskrovniibv@gmail.com>
2025-01-21 17:45:08 +0000

    Improve draft for problem #2017

--- 2017/2017-1.py
+++ 2017/2017-1.py
@@ -1,3 +1,16 @@
+#! /usr/bin/python python python3
+
+from typing import List
+
+
 class Solution:
     def gridGame(self, grid: List[List[int]]) -> int:
-        
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
