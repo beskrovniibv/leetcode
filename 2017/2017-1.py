9e4d3150b0412ffdb68439122cc03ada1910761c
boris <beskrovniibv@gmail.com>
2025-01-21 19:27:03 +0000

    Add solution for problem #2017

--- 2017/2017-1.py
+++ 2017/2017-1.py
@@ -5,7 +5,18 @@ from typing import List
 
 class Solution:
     def gridGame(self, grid: List[List[int]]) -> int:
-        pass
+        n = len(grid[0])
+        psum0 = [0] + [0]*n
+        psum1 = [0] + [0]*n
+        for i in range(n):
+            psum0[i + 1] = psum0[i] + grid[0][i]
+            psum1[i + 1] = psum1[i] + grid[1][i]
+        result = int(10e9) + 1
+        for i in range(1, n + 1):
+            v1 = psum0[-1] - psum0[i]
+            v2 = psum1[-1] - (psum1[-1] - psum1[i - 1])
+            result = min(result, max(v1, v2))
+        return result
 
 
 def main():
@@ -19,6 +30,9 @@ def main():
         (
             [[1, 3, 1, 15], [1, 3, 3, 1]], 7
         ),
+        (
+            [[5, 12, 14], [20, 7, 17]], 20
+        )
     )
     solution = Solution()
     for idx, example in enumerate(examples):
