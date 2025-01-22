6b31d98c65b6510f76d21f95bbd440940dc844a8
boris <beskrovniibv@gmail.com>
2025-01-22 11:32:44 +0000

    Add solution for problem #1765

--- 1765/1765-1.py
+++ 1765/1765-1.py
@@ -1,11 +1,32 @@
 #! /usr/bin/python python python3
 
+from collections import deque
 from typing import List
 
 
 class Solution:
     def highestPeak(self, isWater: List[List[int]]) -> List[List[int]]:
-        pass
+        queue = deque()
+        row_num, col_num = len(isWater), len(isWater[0])
+        result = [[-1]*col_num for _ in range(row_num)]
+        for row in range(row_num):
+            for col in range(col_num):
+                if isWater[row][col] == 1:
+                    queue.append((row, col, 0))
+        dirs = ((-1, 0), (0, -1), (+1, 0), (0, +1))
+        while queue:
+            row, col, value = queue.popleft()
+            if value == 0:
+                result[row][col] = 0
+            for dx, dy in dirs:
+                new_row, new_col = row + dy, col + dx
+                if new_row >= 0 and new_row < row_num and new_col >= 0 and new_col < col_num:
+                    if isWater[new_row][new_col] == 1:
+                        continue
+                    if result[new_row][new_col] == -1 or result[new_row][new_col] > value + 1:
+                        result[new_row][new_col] = value + 1
+                        queue.append((new_row, new_col, value + 1))
+        return result
 
 
 def main():
