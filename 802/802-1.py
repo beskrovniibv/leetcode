9bc3d5ea0ddbe9dfa8413e19e0328db9f5c6d2cf
boris <beskrovniibv@gmail.com>
2025-01-24 17:02:46 +0000

    Improve draft for problem #802

--- 802/802-1.py
+++ 802/802-1.py
@@ -9,7 +9,21 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            [[1, 2], [2, 3], [5], [0], [5], [], []], [2, 4, 5, 6]
+        ),
+        (
+            [[1, 2, 3, 4], [1, 2], [3, 4], [0, 4], []], [4]
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        graph, expected = example
+        got = solution.eventualSafeNodes(
+            graph=graph
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
