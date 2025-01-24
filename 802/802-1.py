7ebc6cad3b33e3e36c7bcf13bfef920cf0b03fe1
boris <beskrovniibv@gmail.com>
2025-01-24 16:59:58 +0000

    Add draft for problem #802

--- 802/802-1.py
+++ 802/802-1.py
@@ -1,3 +1,16 @@
+#! /usr/bin/python python python3
+
+from typing import List
+
+
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
