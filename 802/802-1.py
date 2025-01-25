d5d31e0ac6276b0cb1f75bdaa87b060faecd37df
boris <beskrovniibv@gmail.com>
2025-01-25 09:59:39 +0000

    Add solution for problem #802

--- 802/802-1.py
+++ 802/802-1.py
@@ -1,15 +1,50 @@
 #! /usr/bin/python python python3
 
+from collections import deque
 from typing import List
 
 
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        pass
+        n = len(graph)
+        indegree = [0] * n
+        adj = [[] for _ in range(n)]
+
+        for i in range(n):
+            for node in graph[i]:
+                adj[node].append(i)
+                indegree[i] += 1
+
+        q = deque()
+        # Push all the nodes with indegree zero in the queue.
+        for i in range(n):
+            if indegree[i] == 0:
+                q.append(i)
+
+        safe = [False] * n
+        while q:
+            node = q.popleft()
+            safe[node] = True
+
+            for neighbor in adj[node]:
+                # Delete the edge "node -> neighbor".
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 0:
+                    q.append(neighbor)
+
+        safeNodes = []
+        for i in range(n):
+            if safe[i]:
+                safeNodes.append(i)
+
+        return safeNodes
 
 
 def main():
     examples = (
+        (
+            [[], [0, 2, 3, 4], [3], [4], []], [0, 1, 2, 3, 4]
+        ),
         (
             [[1, 2], [2, 3], [5], [0], [5], [], []], [2, 4, 5, 6]
         ),
