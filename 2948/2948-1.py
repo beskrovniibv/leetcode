6f79ce981dabed410df197b1c47d81aa08efc258
boris <beskrovniibv@gmail.com>
2025-01-25 10:11:20 +0000

    Add draft for problem #2948

--- 2948/2948-1.py
+++ 2948/2948-1.py
@@ -1,3 +1,15 @@
+#! /usr/bin/python python python3
+
+from typing import List
+
 class Solution:
     def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
-        
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
