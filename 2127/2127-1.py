78c918d5a3e3d8ff54b1d7ec026a5c0341a91dec
boris <beskrovniibv@gmail.com>
2025-01-26 18:42:13 +0000

    Improve draft for problem #2127

--- 2127/2127-1.py
+++ 2127/2127-1.py
@@ -9,7 +9,24 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            [2, 2, 1, 2], 3
+        ),
+        (
+            [1, 2, 0], 3
+        ),
+        (
+            [3, 0, 1, 4, 1], 4
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        favorite, expected = example
+        got = solution.maximumInvitations(
+            favorite=favorite
+        )
+        assert got == expected, f"Error in sample {idx+ 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
