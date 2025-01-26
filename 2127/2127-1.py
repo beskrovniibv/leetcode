afe5f4f5cb691e18918d9d83908fae942e27f9fa
boris <beskrovniibv@gmail.com>
2025-01-26 18:39:01 +0000

    Add draft for problem #2127

---
+++ 2127/2127-1.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python python python3
+
+from typing import List
+
+
+class Solution:
+    def maximumInvitations(self, favorite: List[int]) -> int:
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
