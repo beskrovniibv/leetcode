67eae56c873b1ae1bcea8f7615c78ae3c1af7805
boris <beskrovniibv@gmail.com>
2025-01-27 08:03:28 +0000

    Incomplete solution for problem #2127

--- 2127/2127-1.py
+++ 2127/2127-1.py
@@ -5,7 +5,34 @@ from typing import List
 
 class Solution:
     def maximumInvitations(self, favorite: List[int]) -> int:
-        pass
+        n = len(favorite)
+        used = [0]*n
+        parents = [None]*n
+        mxc = 0
+
+        def dfs(v):
+            global mxc
+            used[v] = 1
+            for to in favorite:
+                if not used[to]:
+                    dfs(to)
+                    parents[to] = v
+                elif used[to] == 1:
+                    cycle = []
+                    while parents[v] != to:
+                        cycle.append(v)
+                        v = parents[v]
+                    cycle.append(to)
+                    mxc = max(mxc, len(cycle))
+                used[v] = 2
+
+        reversed = [[] for _ in range(n)]
+        for i in range(n):
+            reversed[favorite[i]].append(i)
+        dfs(0)
+        print(mxc)
+        print(reversed)
+
 
 
 def main():
