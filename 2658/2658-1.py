c2b657b45d366afcdcfecf9814399c2ebb20be32
boris <beskrovniibv@gmail.com>
2025-01-28 00:48:15 +0000

    Improve draft for problem #2658

--- 2658/2658-1.py
+++ 2658/2658-1.py
@@ -4,12 +4,26 @@ from typing import List
 
 
 class Solution:
-    def findMaxFish(self, grid: List[List[int]]) -> int:
+    def findMaxFish(self,  grid: List[List[int]]) -> int:
         pass
 
 
 def main():
-    pass
+    examples = (
+        (
+            [[0, 2, 1, 0], [4, 0, 0, 3], [1, 0, 0, 4], [0, 3, 2, 0]], 7
+        ),
+        (
+            [[1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 1]], 1
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        grid, expected = example
+        got = solution.findMaxFish(
+            grid=grid,
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
