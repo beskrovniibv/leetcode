ca55c9eaf9d50aa58cdfe8f9b0e0eda4386be047
boris <beskrovniibv@gmail.com>
2025-01-29 17:43:36 +0000

    Improve draft for problem #684

--- 684/684-1.py
+++ 684/684-1.py
@@ -9,7 +9,21 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            [[1, 2], [1, 3], [2, 3]], [2, 3]
+        ),
+        (
+            [[1, 2], [2, 3], [3, 4], [1, 4], [1, 5]], [1, 4]
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        edges, expected = example
+        got = solution.findRedundantConnection(
+            edges=edges
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
