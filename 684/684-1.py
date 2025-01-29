c3ed0aa0a217443687c8a6f419abbccd72f7cfd9
boris <beskrovniibv@gmail.com>
2025-01-29 17:39:53 +0000

    Add draft for problem #684

--- 684/684-1.py
+++ 684/684-1.py
@@ -1,3 +1,16 @@
+#! /usr/bin/env python
+
+from typing import List
+
+
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
