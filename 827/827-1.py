a58fd31be66ad9b9c6799cd9f8b2db5053a94e97
boris <beskrovniibv@gmail.com>
2025-01-31 11:58:59 +0000

    Add draft for problem #827

--- 827/827-1.py
+++ 827/827-1.py
@@ -1,3 +1,16 @@
+#! /usr/bin/env python
+
+from typing import List
+
+
 class Solution:
     def largestIsland(self, grid: List[List[int]]) -> int:
-        
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
