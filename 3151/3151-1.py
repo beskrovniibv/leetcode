081bce94408dee18342aecfe749426887c410d5c
boris <beskrovniibv@gmail.com>
2025-02-01 00:17:53 +0000

    Improve draft for problem #3151

--- 3151/3151-1.py
+++ 3151/3151-1.py
@@ -9,7 +9,24 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            [1], True
+        ),
+        (
+            [2, 1, 4], True
+        ),
+        (
+            [4, 3, 1, 6], False
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        nums, expected = example
+        got = solution.isArraySpecial(
+            nums=nums
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
