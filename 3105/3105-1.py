71911c7e6ebf0cfe2ef63160c006ba3992878d34
boris <beskrovniibv@gmail.com>
2025-02-03 12:51:14 +0000

    Add solution for problem #3105

--- 3105/3105-1.py
+++ 3105/3105-1.py
@@ -5,7 +5,18 @@ from typing import List
 
 class Solution:
     def longestMonotonicSubarray(self, nums: List[int]) -> int:
-        pass
+        n = len(nums)
+        mns = [1]*n
+        mxs = [1]*n
+        mx, mn = 1, 1
+        for i in range(1, n):
+            if nums[i] > nums[i - 1]:
+                mxs[i] = mxs[i - 1] + 1
+                mx = max(mx, mxs[i])
+            if nums[i] < nums[i - 1]:
+                mns[i] = mns[i - 1] + 1
+                mn = max(mn, mns[i])
+        return max(mx, mn)
 
 
 def main():
