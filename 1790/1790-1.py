4aea26b56f984d6407512095b61cce1ea1569ece
boris <beskrovniibv@gmail.com>
2025-02-05 00:40:35 +0000

    Add draft for problem #1790

---
+++ 1790/1790-1.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+class Solution:
+    def areAlmostEqual(self, s1: str, s2: str) -> bool:
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
