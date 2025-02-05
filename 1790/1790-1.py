d0c10f171c9d851f3c96796454434d2f0b4ee3d4
boris <beskrovniibv@gmail.com>
2025-02-05 00:51:11 +0000

    Add solution for problem #1790

--- 1790/1790-1.py
+++ 1790/1790-1.py
@@ -2,11 +2,45 @@
 
 class Solution:
     def areAlmostEqual(self, s1: str, s2: str) -> bool:
-        pass
+        if s1 == s2:
+            return True
+        n = len(s1)
+        c, idx1, idx2 = 0, -1, -1
+        for i in range(n):
+            if s1[i] != s2[i]:
+                c += 1
+                if c > 2:
+                    return False
+                if c == 1:
+                    idx1 = i
+                else:
+                    idx2 = i
+        return s1[idx1] == s2[idx2] and s1[idx2] == s2[idx1]
 
 
 def main():
-    pass
+    examples = (
+        (
+            "npv", "zpn", False
+        ),
+        (
+            "bank", "kanb", True
+        ),
+        (
+            "attack", "defend", False
+        ),
+        (
+            "kelb", "kelb", True
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        s1, s2, expected = example
+        got = solution.areAlmostEqual(
+            s1=s1,
+            s2=s2
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
