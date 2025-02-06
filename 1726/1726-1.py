9479f13fc4e7337360290c99a51c6a6f629059ef
boris <beskrovniibv@gmail.com>
2025-02-06 16:23:47 +0000

    Improve draft for problem #1726

--- 1726/1726-1.py
+++ 1726/1726-1.py
@@ -4,12 +4,26 @@ from typing import List
 
 
 class Solution:
-    def tupleSameProduct(self, nums: List[int]) -> int:
+    def tupleSameProduct(self,  nums: List[int]) -> int:
         pass
 
 
 def main():
-    pass
+    examples = (
+        (
+            [2, 3, 4, 6], 8
+        ),
+        (
+            [1, 2, 4, 5, 10], 16
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        nums, expected = example
+        got = solution.tupleSameProduct(
+            nums=nums,
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
