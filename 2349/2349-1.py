5bdaeaeeb4dda4c5e0aa833203959cbc96f24dac
boris <beskrovniibv@gmail.com>
2025-02-08 07:27:30 +0000

    Add draft for problem #2349

---
+++ 2349/2349-1.py
@@ -0,0 +1,18 @@
+class NumberContainers:
+
+    def __init__(self):
+        pass
+
+    def change(self, index: int, number: int) -> None:
+        pass
+
+    def find(self, number: int) -> int:
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
