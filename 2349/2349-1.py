6594100f8d5ec101bf2c0179e019787080337b00
boris <beskrovniibv@gmail.com>
2025-02-08 07:54:06 +0000

    Add solution for problem #2349 (TL)

--- 2349/2349-1.py
+++ 2349/2349-1.py
@@ -1,18 +1,47 @@
 class NumberContainers:
 
     def __init__(self):
-        pass
+        self.indices = {}
+        self.numbers = {}
+        self.mins = {}
 
     def change(self, index: int, number: int) -> None:
-        pass
+        old = self.indices.get(index)
+        if old:
+            self.numbers[old].remove(index)
+        l = self.numbers.get(number, [])
+        l.append(index)
+        self.numbers[number] = sorted(l)
+        self.indices[index] = number
 
     def find(self, number: int) -> int:
-        pass
+        index = self.numbers.get(number)
+        if index:
+            index = index[0]
+        else:
+            index = -1
+        return index
 
 
 def main():
-    pass
-
+    examples = (
+        (
+            ["NumberContainers", "find", "change", "change", "change", "change", "find", "change", "find"], [[], [10], [2, 10], [1, 10], [3, 10], [5, 10], [10], [1, 20], [10]], [None, -1, None, None, None, None, 1, None, 2]
+        ),
+    )
+    for idx, example in enumerate(examples):
+        cmds, args, expected = example
+        got = []
+        for cmd, arg in zip(cmds, args):
+            if cmd == "NumberContainers":
+                numbercontainers = NumberContainers()
+                got.append(None)
+            elif cmd == "find":
+                got.append(numbercontainers.find(*arg))
+            elif cmd == "change":
+                numbercontainers.change(*arg)
+                got.append(None)
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 if __name__ == "__main__":
     main()
