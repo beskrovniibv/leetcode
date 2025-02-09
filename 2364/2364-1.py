372e5d08fdaf2a386ef066d1267b55f6cc1e2d54
boris <beskrovniibv@gmail.com>
2025-02-09 01:32:01 +0000

    Add solution for problem #2364

--- 2364/2364-1.py
+++ 2364/2364-1.py
@@ -5,11 +5,32 @@ from typing import List
 
 class Solution:
     def countBadPairs(self, nums: List[int]) -> int:
-        pass
+        n = len(nums)
+        diffs = {}
+        result = 0
+        for i in range(n):
+            c = diffs.get(i - nums[i], 0)
+            result += i - c
+            diffs[i - nums[i]] = c + 1
+        return result
 
 
 def main():
-    pass
+    examples = (
+        (
+            [4, 1, 3, 3], 5
+        ),
+        (
+            [1, 2, 3, 4, 5], 0
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        nums, expected = example
+        got = solution.countBadPairs(
+            nums=nums
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
