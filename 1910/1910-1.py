f0c005f108ed90351c7ffc9531df594067ef5595
boris <beskrovniibv@gmail.com>
2025-02-11 07:22:01 +0000

    Add draft for problem #1910

--- 1910/1910-1.py
+++ 1910/1910-1.py
@@ -1,3 +1,13 @@
+#! /usr/bin/env python
+
 class Solution:
     def removeOccurrences(self, s: str, part: str) -> str:
-        
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
