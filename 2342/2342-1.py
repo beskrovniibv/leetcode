f282fa1609b59007f57e1693a98670d3d169e694
boris <beskrovniibv@gmail.com>
2025-02-12 10:36:15 +0000

    Improve draft for problem #2342

--- 2342/2342-1.py
+++ 2342/2342-1.py
@@ -9,7 +9,21 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            [18, 43, 36, 13, 7], 54
+        ),
+        (
+            [10, 12, 19, 14], -1
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        nums, expected = example
+        got = solution.maximumSum(
+            nums=nums
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
