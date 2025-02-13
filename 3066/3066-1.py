32995338016ef6162cf4c915a100000d536d3675
boris <beskrovniibv@gmail.com>
2025-02-13 01:09:16 +0000

    Add draft for problem #3066

--- 3066/3066-1.py
+++ 3066/3066-1.py
@@ -1,3 +1,16 @@
+#! /usr/bin/env python
+
+from typing import List
+
+
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
-        
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
