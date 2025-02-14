bc47ad7b28a585cdf5b0d9a8b3319b5a28a20c66
boris <beskrovniibv@gmail.com>
2025-02-14 18:09:27 +0000

    Add solution for problem #3066

--- 3066/3066-1.py
+++ 3066/3066-1.py
@@ -1,11 +1,38 @@
 #! /usr/bin/env python
 
+from collections import deque
 from typing import List
 
 
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
-        pass
+        result = 0
+        q1 = deque(sorted(nums))
+        q2 = deque([])
+        data = [q1.popleft(), q1.popleft()]
+        j1, j2 = data
+        b = False
+        while j1 < k or j2 < k:
+            result += 1
+            v = min(j1, j2)*2 + max(j1, j2)
+            q2.append(v)
+            for i in range(2):
+                if q1 and q2:
+                    if q1[0] < q2[0]:
+                        data[i] = q1.popleft()
+                    else:
+                        data[i] = q2.popleft()
+                elif q1:
+                    data[i] = q1.popleft()
+                elif q2:
+                    data[i] = q2.popleft()
+                else:
+                    b = True
+                    break
+            if b:
+                break
+            j1, j2 = data
+        return result
 
 
 def main():
@@ -15,6 +42,9 @@ def main():
         ),
         (
             [1, 1, 2, 4, 9], 20, 4
+        ),
+        (
+            [999999999, 999999999, 999999999], 1000000000, 2
         ),
     )
     solution = Solution()
