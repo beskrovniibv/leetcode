b14bd78816935a9b792795ebda8880fcbacf2e3a
boris <beskrovniibv@gmail.com>
2025-02-15 12:09:49 +0000

    Improve draft for problem #2698

--- 2698/2698-1.py
+++ 2698/2698-1.py
@@ -1,3 +1,27 @@
+#! /usr/bin/env python
+
 class Solution:
     def punishmentNumber(self, n: int) -> int:
-        
+        pass
+
+
+def main():
+    examples = (
+        (
+            10, 182
+        ),
+        (
+            37, 1478
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        n, expected = example
+        got = solution.punishmentNumber(
+            n=n
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
+
+
+if __name__ == "__main__":
+    main()
