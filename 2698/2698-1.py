1e2d5e9ac1acfacc08ba06c2c037dadefe8efa37
boris <beskrovniibv@gmail.com>
2025-02-17 02:14:47 +0000

    Added draft for problem #2698

--- 2698/2698-1.py
+++ 2698/2698-1.py
@@ -23,5 +23,18 @@ def main():
         assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
+def check(number):
+    def check_(number, sq, _break, d):
+        if _break:
+            return True
+        return check_(number, )
+        pass
+    return check_(number, number**2, False, 10)
+    pass
+
+
 if __name__ == "__main__":
+    pre = [False]*1000
+    for i in range(1001):
+        pre[i] = check(i)
     main()
