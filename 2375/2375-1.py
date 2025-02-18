ba993d41f156eee0d563ee1ccda8ab788833befe
boris <beskrovniibv@gmail.com>
2025-02-18 18:15:59 +0000

    Improve draft for problem #2375

--- 2375/2375-1.py
+++ 2375/2375-1.py
@@ -6,7 +6,21 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            "IIIDIDDD", "123549876"
+        ),
+        (
+            "DDD", "4321"
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        pattern, expected = example
+        got = solution.smallestNumber(
+            pattern=pattern
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
