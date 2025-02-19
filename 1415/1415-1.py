02ffda6712754448336cef1a1dfd962aa5f1ce1e
boris <beskrovniibv@gmail.com>
2025-02-19 16:39:46 +0000

    Improve draft for problem #1415

--- 1415/1415-1.py
+++ 1415/1415-1.py
@@ -6,7 +6,25 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            1, 3, "c"
+        ),
+        (
+            1, 4, ""
+        ),
+        (
+            3, 9, "cab"
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        n, k, expected = example
+        got = solution.getHappyString(
+            n=n,
+            k=k
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
