0c074397b837bd17adb00557f4b323022c768c66
boris <beskrovniibv@gmail.com>
2025-02-20 05:19:47 +0000

    Add draft for problem #1980

--- 1980/1980-1.py
+++ 1980/1980-1.py
@@ -1,3 +1,16 @@
+#! /usr/bin/env python
+
+from typing import List
+
+
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
-        
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
