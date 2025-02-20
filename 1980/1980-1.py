ada98ec63f609c84774e48d5393a834c8cabd0b5
boris <beskrovniibv@gmail.com>
2025-02-20 23:14:22 +0000

    Add solution for problem #1980

--- 1980/1980-1.py
+++ 1980/1980-1.py
@@ -5,11 +5,32 @@ from typing import List
 
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
-        pass
+        n = len(nums[0])
+        result = ""
+        for i in range(n):
+            result += ("0", "1")[nums[i][i] == "0"]
+        return result
 
 
 def main():
-    pass
+    examples = (
+        (
+            ["01", "10"], "11"
+        ),
+        (
+            ["00", "01"], "10"
+        ),
+        (
+            ["111", "011", "001"], "000"
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        nums, expected = example
+        got = solution.findDifferentBinaryString(
+            nums=nums
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
