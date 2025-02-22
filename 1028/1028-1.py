6584abb894303208b5e17184cb4494c6f238fd63
boris <beskrovniibv@gmail.com>
2025-02-22 13:22:51 +0000

    Add solution for problem #1028

--- 1028/1028-1.py
+++ 1028/1028-1.py
@@ -10,14 +10,44 @@ class TreeNode:
         self.left = left
         self.right = right
 
+    def __repr__(self):
+        l = f"{self.left.val}" if self.left else "None"
+        r = f"{self.right.val}" if self.right else "None"
+        return f"({self.val}: ({l}), ({r}))"
+
 
 class Solution:
     def recoverFromPreorder(self, traversal: str) -> Optional[TreeNode]:
-        pass
+        n = len(traversal)
+        st = []
+        i = 0
+        while i < n:
+            value = 0
+            depth = 0
+            while i < n and traversal[i] == "-":
+                depth += 1
+                i += 1
+            while i < n and traversal[i] in '0123456789':
+                value = value*10 + int(traversal[i])
+                i += 1
+            node = TreeNode(value)
+            while len(st) > depth:
+                st.pop()
+            if st:
+                parent = st[-1]
+                if not parent.left:
+                    parent.left = node
+                else:
+                    parent.right = node
+            st.append(node)
+        return st[0]
 
 
 def main():
-    pass
+    solution = Solution()
+    solution.recoverFromPreorder(
+        traversal="1-2--3--4-5--6--7"
+    )
 
 
 if __name__ == "__main__":
