eeafc6a32cd8fb57646aaf163da376afd143f32e
boris <beskrovniibv@gmail.com>
2025-02-25 13:21:01 +0000

    Improve draft for problem #1524

--- 1524/1524-1.py
+++ 1524/1524-1.py
@@ -9,7 +9,24 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            [1, 3, 5], 4
+        ),
+        (
+            [2, 4, 6], 0
+        ),
+        (
+            [1, 2, 3, 4, 5, 6, 7], 16
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        arr, expected = example
+        got = solution.numOfSubarrays(
+            arr=arr
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
