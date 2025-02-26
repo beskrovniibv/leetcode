77138a57b0f416547ac53f23810b8557a266c830
boris <beskrovniibv@gmail.com>
2025-02-26 19:01:52 +0000

    Improve draft for problem #1749

--- 1749/1749-1.py
+++ 1749/1749-1.py
@@ -9,7 +9,21 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            [1, -3, 2, 3, -4], 5
+        ),
+        (
+            [2, -5, 1, -4, 3, -2], 8
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        nums, expected = example
+        got = solution.maxAbsoluteSum(
+            nums=nums
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
