b833024a134c93fb3c3e3404a86e348608da17ed
boris <beskrovniibv@gmail.com>
2025-02-28 17:34:17 +0000

    Add draft for problem #1092

---
+++ 1092/1092-1.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+class Solution:
+    def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
