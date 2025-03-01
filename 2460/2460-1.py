118e9a86d3a07c6858f22251396ef7f8820d2c96
boris <beskrovniibv@gmail.com>
2025-03-01 05:04:34 +0000

    Improve draft for problem #2460

--- 2460/2460-1.py
+++ 2460/2460-1.py
@@ -9,7 +9,21 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            [1, 2, 2, 1, 1, 0], [1, 4, 2, 0, 0, 0]
+        ),
+        (
+            [0, 1], [1, 0]
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        nums, expected = example
+        got = solution.applyOperations(
+            nums=nums
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
