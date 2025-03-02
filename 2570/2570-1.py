9f3b08f35688baba9ac9652a6c936e72279d78b7
boris <beskrovniibv@gmail.com>
2025-03-02 12:24:35 +0000

    Add draft for problem #2570

--- 2570/2570-1.py
+++ 2570/2570-1.py
@@ -1,3 +1,16 @@
+#! /usr/bin/env python
+
+from typing import List
+
+
 class Solution:
     def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
-        
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
