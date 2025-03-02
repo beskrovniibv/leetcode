a0c6a68a203c7ed3be51f6f827a383678449355f
boris <beskrovniibv@gmail.com>
2025-03-02 12:28:08 +0000

    Improve draft for problem #2570

--- 2570/2570-1.py
+++ 2570/2570-1.py
@@ -9,7 +9,22 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            [[1, 2], [2, 3], [4, 5]], [[1, 4], [3, 2], [4, 1]], [[1, 6], [2, 3], [3, 2], [4, 6]]
+        ),
+        (
+            [[2, 4], [3, 6], [5, 5]], [[1, 3], [4, 3]], [[1, 3], [2, 4], [3, 6], [4, 3], [5, 5]]
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        nums1, nums2, expected = example
+        got = solution.mergeArrays(
+            nums1=nums1,
+            nums2=nums2
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
