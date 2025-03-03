f216b438cafbc372f082388b88d1aa70f5db77e6
boris <beskrovniibv@gmail.com>
2025-03-03 13:19:15 +0000

    Add draft for problem #2161

---
+++ 2161/2161-1.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+
+from typing import List
+
+
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
