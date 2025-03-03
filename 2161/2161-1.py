79e9ce508d376a4c297f03e460ea8a078fa86a09
boris <beskrovniibv@gmail.com>
2025-03-03 13:21:56 +0000

    Improve draft for problem #2161

--- 2161/2161-1.py
+++ 2161/2161-1.py
@@ -9,7 +9,22 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            [9, 12, 5, 10, 14, 3, 10], 10, [9, 5, 3, 10, 10, 12, 14]
+        ),
+        (
+            [-3, 4, 3, 2], 2, [-3, 2, 4, 3]
+        ),
+    )
+    solution = Solution()
+    for idx, exmaple in enumerate(examples):
+        nums, pivot, expected = exmaple
+        got = solution.pivotArray(
+            nums=nums,
+            pivot=pivot
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
