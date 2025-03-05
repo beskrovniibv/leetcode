4b52b7e7a1e2530332e317b3eda6fdcc15461833
boris <beskrovniibv@gmail.com>
2025-03-05 00:46:43 +0000

    Add solution for problem #2579

--- 2579/2579-1.py
+++ 2579/2579-1.py
@@ -1,12 +1,31 @@
 #! /usr/bin/env python
 
 class Solution:
+    def count(self, n):
+        if n == 1:
+            return 1
+        return 2*n + 2*(n - 2) + self.count(n - 1)
+
     def coloredCells(self, n: int) -> int:
-        pass
+        return self.count(n)
 
 
 def main():
-    pass
+    examples = (
+        (
+            1, 1
+        ),
+        (
+            2, 5
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        n, expected = example
+        got = solution.coloredCells(
+            n=n
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
