1a800792fd60d1df07ea102f29c23b2d9fbbaa4c
boris <beskrovniibv@gmail.com>
2025-03-06 16:24:39 +0000

    Improve draft for problem #2965

--- 2965/2965-1.py
+++ 2965/2965-1.py
@@ -4,12 +4,26 @@ from typing import List
 
 
 class Solution:
-    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+    def findMissingAndRepeatedValues(self,  grid: List[List[int]]) -> List[int]:
         pass
 
 
 def main():
-    pass
+    examples = (
+        (
+            [[1, 3], [2, 2]], [2, 4]
+        ),
+        (
+            [[9, 1, 7], [8, 9, 2], [3, 4, 6]], [9, 5]
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        grid, expected = example
+        got = solution.findMissingAndRepeatedValues(
+            grid=grid
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
