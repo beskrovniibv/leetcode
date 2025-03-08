81040d153c532c832a3ebb68e399bc3c0e642e30
boris <beskrovniibv@gmail.com>
2025-03-08 19:35:17 +0000

    Improve draft for problem #2379

--- 2379/2379-1.py
+++ 2379/2379-1.py
@@ -6,7 +6,22 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            "WBBWWBBWBW", 7, 3
+        ),
+        (
+            "WBWBBBW", 2, 0
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        blocks, k, expected = example
+        got = solution.minimumRecolors(
+            blocks=blocks,
+            k=k,
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
