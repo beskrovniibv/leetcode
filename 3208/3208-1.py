a65f242287dc91a75139a19d143991c37650bff5
boris <beskrovniibv@gmail.com>
2025-03-09 17:02:42 +0000

    Improve draft for problem #3208

--- 3208/3208-1.py
+++ 3208/3208-1.py
@@ -9,7 +9,25 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            [0, 1, 0, 1, 0], 3, 3
+        ),
+        (
+            [0, 1, 0, 0, 1, 0, 1], 6, 2
+        ),
+        (
+            [1, 1, 0, 1], 4, 0
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        colors, k, expected = example
+        got = solution.numberOfAlternatingGroups(
+            colors=colors,
+            k=k,
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
