f6194e45c575e9b0391af727be148abbf34db6d8
boris <beskrovniibv@gmail.com>
2025-03-10 14:10:06 +0000

    Improve draft for problem #3306

--- 3306/3306-1.py
+++ 3306/3306-1.py
@@ -6,7 +6,25 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            "aeioqq", 1, 0
+        ),
+        (
+            "aeiou", 0, 1
+        ),
+        (
+            "ieaouqqieaouqq", 1, 3
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        word, k, expected = example
+        got = solution.countOfSubstrings(
+            word=word,
+            k=k
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
