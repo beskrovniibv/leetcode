3067a291bdff44159d5ef32a426c2828a9cd4d8b
boris <beskrovniibv@gmail.com>
2025-03-12 11:59:31 +0000

    Improve draft for problem #2529

--- 2529/2529-1.py
+++ 2529/2529-1.py
@@ -9,7 +9,24 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            [-2, -1, -1, 1, 2, 3], 3
+        ),
+        (
+            [-3, -2, -1, 0, 0, 1, 2], 3
+        ),
+        (
+            [5, 20, 66, 1314], 4
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        nums, expected = example
+        got = solution.maximumCount(
+            nums=nums,
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
