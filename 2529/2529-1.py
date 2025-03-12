7360d6abb111315b8509cf8073aa3c210b3df74a
boris <beskrovniibv@gmail.com>
2025-03-12 11:56:31 +0000

    Add draft for problem #2529

--- 2529/2529-1.py
+++ 2529/2529-1.py
@@ -1,10 +1,16 @@
+#! /usr/bin/env python
+
+from typing import List
+
+
 class Solution:
     def maximumCount(self, nums: List[int]) -> int:
-        n = len(nums)
-        pos, neg = 0, 0
-        for i in range(n):
-            if nums[i] < 0 :
-                neg += 1
-            elif nums[i] > 0:
-                pos += 1
-        return max(pos, neg)
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
