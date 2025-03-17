bf9373106d5efa798cf437e7acf3a89f3cb33078
boris <beskrovniibv@gmail.com>
2025-03-17 01:31:23 +0000

    Add solution for problem #2529

--- 2529/2529-1.py
+++ 2529/2529-1.py
@@ -8,7 +8,7 @@ class Solution:
         n = len(nums)
         pos, neg = 0, 0
         for i in range(n):
-            if nums[i] < 0 :
+            if nums[i] < 0:
                 neg += 1
             elif nums[i] > 0:
                 pos += 1
