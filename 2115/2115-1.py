49807bf65a9c2515547ee3b26064ed7c16683cf3
boris <beskrovniibv@gmail.com>
2025-03-21 06:43:39 +0000

    Add draft for problem #2115

---
+++ 2115/2115-1.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+
+from typing import List
+
+
+class Solution:
+    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
