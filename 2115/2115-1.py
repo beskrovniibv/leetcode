ba4e152b0da096b9f92f4715c32478f409b9ff0c
boris <beskrovniibv@gmail.com>
2025-03-21 09:16:36 +0000

    Improve draft for problem #2115

--- 2115/2115-1.py
+++ 2115/2115-1.py
@@ -9,7 +9,26 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            ["bread"], [["yeast", "flour"]], ["yeast", "flour", "corn"], ["bread"]
+        ),
+        (
+            ["bread", "sandwich"], [["yeast", "flour"], ["bread", "meat"]], ["yeast", "flour", "meat"], ["bread", "sandwich"]
+        ),
+        (
+            ["bread", "sandwich", "burger"], [["yeast", "flour"], ["bread", "meat"], ["sandwich", "meat", "bread"]], ["yeast", "flour", "meat"], ["bread", "sandwich", "burger"]
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        recipes, ingredients, supplies, expected = example
+        got = solution.findAllRecipes(
+            recipes=recipes,
+            ingredients=ingredients,
+            supplies=supplies
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
