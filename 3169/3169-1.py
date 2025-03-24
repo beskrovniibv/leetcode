72793e5c22395c223d6e88465c444cda3e0c9c65
boris <beskrovniibv@gmail.com>
2025-03-24 18:16:26 +0000

    Add draft for problem #3169

--- 3169/3169-1.py
+++ 3169/3169-1.py
@@ -1,3 +1,16 @@
+#! /usr/bin/env python
+
+from typing import List
+
+
 class Solution:
     def countDays(self, days: int, meetings: List[List[int]]) -> int:
-        
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
