3f265ffcd2e091692b9b4a30d5e6d7fb5f2765d6
boris <beskrovniibv@gmail.com>
2025-03-27 21:17:57 +0000

    Improve draft for problem #2780

--- 2780/2780-1.py
+++ 2780/2780-1.py
@@ -9,7 +9,24 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            [1, 2, 2, 2], 2
+        ),
+        (
+            [2, 1, 3, 1, 1, 1, 7, 1, 2, 1], 4
+        ),
+        (
+            [3, 3, 3, 3, 7, 2, 2], -1
+        )
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        nums, expected = example
+        got = solution.minimumIndex(
+            nums=nums
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
