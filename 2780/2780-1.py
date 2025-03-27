2595726602d95eb75198ba69e167133ef3210f33
boris <beskrovniibv@gmail.com>
2025-03-27 21:14:21 +0000

    Add draft for problem #2780

--- 2780/2780-1.py
+++ 2780/2780-1.py
@@ -1,3 +1,16 @@
+#! /usr/bin/env python
+
+from typing import List
+
+
 class Solution:
     def minimumIndex(self, nums: List[int]) -> int:
-        
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
