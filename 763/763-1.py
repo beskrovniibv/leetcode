41210b2047a0b8c7249d21dc57bf85e3fccb34ca
boris <beskrovniibv@gmail.com>
2025-03-30 17:30:11 +0000

    Add solution for problem #763

--- 763/763-1.py
+++ 763/763-1.py
@@ -5,11 +5,35 @@ from typing import List
 
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        pass
+        n = len(s)
+        chars = {}
+        for idx, ch in enumerate(s):
+            f, l = chars.get(ch, [-1, -1])
+            if f == -1:
+                f = idx
+            l = max(l, idx)
+            chars[ch] = [f, l]
+        result = []
+        end = None
+        for i in range(n):
+            if end is not None and i > end:
+                result.append(i)
+                end = None
+            c = s[i]
+            f, l = chars[c]
+            end = max(end and end or l, l)
+        if end:
+            result.append(end + 1)
+        for i in range(len(result) - 1, 0, -1):
+            result[i] = result[i] - result[i - 1]
+        return result
 
 
 def main():
     examples = (
+        (
+            "caedbdedda", [1, 9]
+        ),
         (
             "ababcbacadefegdehijhklij", [9, 7, 8]
         ),
