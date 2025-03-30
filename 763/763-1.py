649fc60cca1d6e5cdcfc121f9d40cb7ede4e5987
boris <beskrovniibv@gmail.com>
2025-03-30 16:57:35 +0000

    Improve draft for problem #763

--- 763/763-1.py
+++ 763/763-1.py
@@ -9,7 +9,21 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            "ababcbacadefegdehijhklij", [9, 7, 8]
+        ),
+        (
+            "eccbbbbdec", [10]
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        s, expected = example
+        got = solution.partitionLabels(
+            s=s
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
