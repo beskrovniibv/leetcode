f57d1a52d066cc59e9559e02d0e9e2fc70220db6
boris <beskrovniibv@gmail.com>
2025-03-30 16:55:00 +0000

    Add draft for problem #763

---
+++ 763/763-1.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+
+from typing import List
+
+
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
