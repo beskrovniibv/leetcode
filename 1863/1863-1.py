d18699d87740faf3d4f4925d84a1613eb507ef6d
boris <beskrovniibv@gmail.com>
2025-04-05 18:56:19 +0000

    Improve draft for problem #1863

--- 1863/1863-1.py
+++ 1863/1863-1.py
@@ -9,7 +9,24 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            [1, 3], 6
+        ),
+        (
+            [5, 1, 6], 28
+        ),
+        (
+            [3, 4, 5, 6, 7, 8], 480
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        nums, expected = example
+        got = solution.subsetXORSum(
+            nums=nums
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
