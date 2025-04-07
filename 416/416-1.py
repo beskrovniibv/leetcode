f9087d6013a02d3945abba0346e9ef6b65ab36a6
boris <beskrovniibv@gmail.com>
2025-04-07 17:10:10 +0000

    Add draft for problem #416

---
+++ 416/416-1.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+
+from typing import List
+
+
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
