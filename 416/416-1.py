c2b1143e6d422604ae7218b8933b985297493e28
boris <beskrovniibv@gmail.com>
2025-04-07 17:15:33 +0000

    Improve draft for problem #416

--- 416/416-1.py
+++ 416/416-1.py
@@ -9,8 +9,21 @@ class Solution:
 
 
 def main():
-    pass
-
+    examples = (
+        (
+            [1, 5, 11, 5], True
+        ),
+        (
+            [1, 2, 3, 5], False
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        nums, expected = example
+        got = solution.canPartition(
+            nums=nums
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 if __name__ == "__main__":
     main()
