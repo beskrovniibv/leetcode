fe14effa7689d52a120922eed878d48f180002d7
boris <beskrovniibv@gmail.com>
2025-04-08 05:21:12 +0000

    Improve draft for problem #3396

--- 3396/3396-1.py
+++ 3396/3396-1.py
@@ -9,7 +9,24 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            [1, 2, 3, 4, 2, 3, 3, 5, 7], 2
+        ),
+        (
+            [4, 5, 6, 4, 4], 2
+        ),
+        (
+            [6, 7, 8, 9], 0
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        nums, expected = example
+        got = solution.minimumOperations(
+            nums=nums
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
