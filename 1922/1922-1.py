4ef14a49accaefeac344d16e2bad2196d81518f1
boris <beskrovniibv@gmail.com>
2025-04-13 12:48:49 +0000

    Improve draft for problem #1922

--- 1922/1922-1.py
+++ 1922/1922-1.py
@@ -9,7 +9,24 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            1, 5
+        ),
+        (
+            4, 400
+        ),
+        (
+            50, 564908303
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        n, expected = example
+        got = solution.countGoodNumbers(
+            n=n
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
