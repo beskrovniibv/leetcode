f560625b8b723258e6b77829605fef087c452717
boris <beskrovniibv@gmail.com>
2025-04-13 13:35:04 +0000

    Add solution for problem #1922

--- 1922/1922-1.py
+++ 1922/1922-1.py
@@ -3,9 +3,20 @@
 MODULO = 10**9 + 7
 
 
+def pow(a: int, b: int) -> int:
+    result = 1
+    while b > 0:
+        if b & 1 == 1:
+            result *= a
+            b -= 1
+        a = a**2 % MODULO
+        b = b // 2
+    return result % MODULO
+
+
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
-        pass
+        return pow(5, (n + 1)//2)*pow(4, n//2) % MODULO
 
 
 def main():
@@ -18,6 +29,9 @@ def main():
         ),
         (
             50, 564908303
+        ),
+        (
+            806166225460393, 643535977
         ),
     )
     solution = Solution()
