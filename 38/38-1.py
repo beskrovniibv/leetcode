3ff7ecbcffa762fa2c8e60b18bac951f16cd7fb6
boris <beskrovniibv@gmail.com>
2025-04-18 16:52:21 +0000

    Improve draft for problem #38

--- 38/38-1.py
+++ 38/38-1.py
@@ -14,7 +14,14 @@ def main():
             1, "1"
         )
     )
-    
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        n, expected = example
+        got = solution.countAndSay(
+            n=1
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
+
 
 if __name__ == "__main__":
     main()
