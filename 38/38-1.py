7ff56848a08e0fee0963d856240d1aea59ff2c30
boris <beskrovniibv@gmail.com>
2025-04-18 17:04:22 +0000

    Add solution for problem #38

--- 38/38-1.py
+++ 38/38-1.py
@@ -2,7 +2,27 @@
 
 class Solution:
     def countAndSay(self, n: int) -> str:
-        pass
+        for i in range(1, n + 1):
+            if i == 1:
+                rle = "1"
+            else:
+                l = None
+                c = 0
+                temp = []
+                for ch in rle:
+                    if l and ch == l:
+                        c += 1
+                    elif not l:
+                        l = ch
+                        c = 1
+                    else:
+                        temp.append(f"{c}{l}")
+                        l = ch
+                        c = 1
+                temp.append(f"{c}{l}")
+                result = "".join(temp)
+                rle = result
+        return rle
 
 
 def main():
@@ -18,7 +38,7 @@ def main():
     for idx, example in enumerate(examples):
         n, expected = example
         got = solution.countAndSay(
-            n=1
+            n=n
         )
         assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
