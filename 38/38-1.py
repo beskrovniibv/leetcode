1d043d8965d303cca3e4e2f7187b0c2c42122053
boris <beskrovniibv@gmail.com>
2025-04-18 16:50:46 +0000

    Add draft for problem #38

--- 38/38-1.py
+++ 38/38-1.py
@@ -1,3 +1,20 @@
+#! /usr/bin/env python
+
 class Solution:
     def countAndSay(self, n: int) -> str:
-        
+        pass
+
+
+def main():
+    examples = (
+        (
+            4, "1211"
+        ),
+        (
+            1, "1"
+        )
+    )
+    
+
+if __name__ == "__main__":
+    main()
