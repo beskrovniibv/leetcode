fe1750cb7ae0021bf6a14574fee148fd7cf1df1f
boris <beskrovniibv@gmail.com>
2025-06-14 07:56:41 +0000

    Add solution for problem #2566

--- 2566/2566-1.py
+++ 2566/2566-1.py
@@ -2,11 +2,36 @@
 
 class Solution:
     def minMaxDifference(self, num: int) -> int:
-        pass
+        s = [c for c in str(num)]
+        mx, mn = {}, {}
+        for c in s:
+            if c != '9':
+                mx[c] = '9'
+                break
+        mn[s[0]] = '0'
+        max, min = 0, 0
+        for c in s:
+            max = max*10 + (int(c) if mx.get(c, None) is None else int(mx[c]))
+            min = min*10 + (int(c) if mn.get(c, None) is None else int(mn[c]))
+        return max - min
 
 
 def main():
-    pass
+    examples = (
+        (
+            11891, 99009
+        ),
+        (
+            90, 99
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        num, expected = example
+        got = solution.minMaxDifference(
+            num=num
+        )
+        assert got == expected, f"Error in sample {idx}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
