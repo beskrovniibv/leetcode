329865e417b3d86d0b0668f2f8604587f0dee159
boris <beskrovniibv@gmail.com>
2025-06-16 21:10:52 +0000

    Add solution for problem #2016

--- 2016/2016-1.py
+++ 2016/2016-1.py
@@ -5,19 +5,30 @@ from typing import List
 
 class Solution:
     def maximumDifference(self, nums: List[int]) -> int:
-        pass
+        n = len(nums)
+        l, r = 0, 1
+        mx = -1
+        while r < n:
+            d = nums[r] - nums[l] if nums[l] < nums[r] else -1
+            mx = max(mx, d)
+            if nums[r] < nums[l]:
+                l = r
+                r = l + 1
+                continue
+            r += 1
+        return mx
 
 
 def main():
     examples = (
         (
-            [7,1,5,4], 4
+            [7, 1, 5, 4], 4
         ),
         (
-            [9,4,3,2], -1
+            [9, 4, 3, 2], -1
         ),
         (
-            [1,5,2,10], 9
+            [1, 5, 2, 10], 9
         ),
     )
     solution = Solution()
