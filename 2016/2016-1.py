8cf9d856610b51127e3e1529aed7f6d6bb688225
boris <beskrovniibv@gmail.com>
2025-06-16 20:57:14 +0000

    Improve draft for problem #2016

--- 2016/2016-1.py
+++ 2016/2016-1.py
@@ -1,12 +1,32 @@
 #! /usr/bin/env python
 
+from typing import List
+
+
 class Solution:
     def maximumDifference(self, nums: List[int]) -> int:
         pass
 
 
 def main():
-    pass
+    examples = (
+        (
+            [7,1,5,4], 4
+        ),
+        (
+            [9,4,3,2], -1
+        ),
+        (
+            [1,5,2,10], 9
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        nums, expected = example
+        got = solution.maximumDifference(
+            nums=nums,
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}"
 
 
 if __name__ == "__main__":
