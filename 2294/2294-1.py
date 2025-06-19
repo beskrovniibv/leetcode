f53c6b61e3f84c5a9473dc5cdd2c049e29605467
boris <beskrovniibv@gmail.com>
2025-06-19 11:36:37 +0000

    Add draft for problem #2294

--- 2294/2294-1.py
+++ 2294/2294-1.py
@@ -7,8 +7,27 @@ class Solution:
     def partitionArray(self, nums: List[int], k: int) -> int:
         pass
 
+
 def main():
-    pass
+    examples = (
+        (
+            [3, 6, 1, 2, 5], 2, 2
+        ),
+        (
+            [1, 2, 3], 1, 2
+        ),
+        (
+            [2, 2, 4, 5], 0, 3
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        nums, k, expected = example
+        got = solution.partitionArray(
+            nums=nums,
+            k=k,
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}"
 
 
 if __name__ == "__main__":
