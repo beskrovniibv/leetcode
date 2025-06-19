c26182aa017433a349c49ef775fadb5f0f66a7bb
boris <beskrovniibv@gmail.com>
2025-06-19 11:32:57 +0000

    Add draft for problem #2294

--- 2294/2294-1.py
+++ 2294/2294-1.py
@@ -1,3 +1,15 @@
+#! /usr/bin/env python
+
+from typing import List
+
+
 class Solution:
     def partitionArray(self, nums: List[int], k: int) -> int:
-        
+        pass
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
