af464f696858fc6ec19b0a8412c589cfedd38ffb
boris <beskrovniibv@gmail.com>
2025-06-21 03:57:10 +0000

    Add solution for problem #3085

--- 3085/3085-1.py
+++ 3085/3085-1.py
@@ -2,11 +2,24 @@
 
 class Solution:
     def minimumDeletions(self, word: str, k: int) -> int:
-        pass
+        d = {}
+        for c in word:
+            d[c] = d.get(c, 0) + 1
+        a = list(d.values())
+        result = len(word)
+        for i in range(len(a)):
+            deleted = 0
+            for j in range(len(a)):
+                if a[i] > a[j]:
+                    deleted += a[j]
+                elif a[j] > a[i] + k:
+                    deleted += a[j] - (a[i] + k)
+            result = min(result, deleted)
+        return result
 
 
 def main():
-    examples = (
+    examples = (    
         (
             "aabcaba", 0, 3
         ),
@@ -24,7 +37,7 @@ def main():
             word=word,
             k=k,
         )
-        assert got == expected, f"Error in sample {idx + 1}: expected {expeted}, got {got}"
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}"
 
 
 if __name__ == "__main__":
