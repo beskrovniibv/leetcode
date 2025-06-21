8c01b1f77b2abbda6a5847e2d4138654ecc3afdb
boris <beskrovniibv@gmail.com>
2025-06-21 01:39:30 +0000

    Improve draft for problem #3085

--- 3085/3085-1.py
+++ 3085/3085-1.py
@@ -6,7 +6,25 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            "aabcaba", 0, 3
+        ),
+        (
+            "dabdcbdcdcd", 2, 2
+        ),
+        (
+            "aaabaaa", 2, 1
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        word, k, expected = example
+        got = solution.minimumDeletions(
+            word=word,
+            k=k,
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expeted}, got {got}"
 
 
 if __name__ == "__main__":
