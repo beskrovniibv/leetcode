10bc0f6bd2e7c30cbd4923d5cb34f1336b01b9c6
boris <beskrovniibv@gmail.com>
2025-06-22 18:42:43 +0000

    Add draft for problem #2138

--- 2138/2138-1.py
+++ 2138/2138-1.py
@@ -1,3 +1,16 @@
+#! /usr/bin/env python
+
+from typing import List
+
+
 class Solution:
     def divideString(self, s: str, k: int, fill: str) -> List[str]:
-        
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
