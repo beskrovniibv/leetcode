e721522c64f95c613de0f4aaefaae67946aeae2d
boris <beskrovniibv@gmail.com>
2025-06-22 18:45:40 +0000

    Improve draft for problem #2138

--- 2138/2138-1.py
+++ 2138/2138-1.py
@@ -9,7 +9,23 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            "abcdefghi", 3, "x", ["abc", "def", "ghi"]
+        ),
+        (
+            "abcdefghij", 3, "x", ["abc", "def", "ghi", "jxx"]
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        s, k, fill, expected = example
+        got = solution.divideString(
+            s=s,
+            k=k,
+            fill=fill,
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}"
 
 
 if __name__ == "__main__":
