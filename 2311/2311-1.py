1e7566583f780fe9c83fe726fd72f37634548cdc
boris <beskrovniibv@gmail.com>
2025-06-26 12:11:40 +0000

    Improve draft for problem #2311

--- 2311/2311-1.py
+++ 2311/2311-1.py
@@ -6,7 +6,22 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            "1001010", 5, 5
+        ),
+        (
+            "00101001", 1, 6
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        s, k, expected = example
+        got = solution.longestSubsequence(
+            s=s,
+            k=k,
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}"
 
 
 if __name__ == "__main__":
