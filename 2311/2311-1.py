dd38f53e97e870e9c0babab5fc71cd92ed8687cc
boris <beskrovniibv@gmail.com>
2025-06-26 11:54:17 +0000

    Add draft for problem #2311

--- 2311/2311-1.py
+++ 2311/2311-1.py
@@ -1,3 +1,13 @@
+#! /usr/bin/env python
+
 class Solution:
     def longestSubsequence(self, s: str, k: int) -> int:
-        
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
