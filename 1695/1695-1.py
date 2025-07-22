8ad816427508454f4d31a1b67c2359ed628b90d3
boris <beskrovniibv@gmail.com>
2025-07-22 18:24:40 +0000

    Improve draft for problem #1695

--- 1695/1695-1.py
+++ 1695/1695-1.py
@@ -8,7 +8,21 @@ class Solution:
 
 
 def main():
-    pass
+    examples = (
+        (
+            [4, 2, 4, 5, 6], 17
+        ),
+        (
+            [5, 2, 1, 2, 5, 2, 1, 2, 5], 8
+        ),
+    )
+    solution = Solution()
+    for idx, example in enumerate(examples):
+        nums, expected = example
+        got = solution.maximumUniqueSubarray(
+            nums=nums,
+        )
+        assert got == expected, f"Error in sample {idx + 1}: expected {expected}, got {got}."
 
 
 if __name__ == "__main__":
